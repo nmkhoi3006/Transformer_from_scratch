2ffccfc1c516c0de80cfb592390f425bc1752321
nmkhoi3006 <khoi30062005@gmail.com>
2025-01-21 16:39:13 +0700

    encoder

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class InputEmbedding(nn.Module):
 
     def __init__(self,
                  vocab_size: int,
-                 d_model: int = 512):
+                 d_model: int):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
@@ -21,8 +21,8 @@ class PositionEmbedding(nn.Module):
 
     def __init__(self, 
                  max_seq_len: int,
-                 d_model: int = 512, 
-                 drop_out: float = 0.1):
+                 d_model: int, 
+                 drop_out: float):
         super().__init__()
         self.max_seq_len = max_seq_len
         self.d_model = d_model
@@ -80,9 +80,9 @@ class Attention(nn.Module):
 class MultiHeadAttention(nn.Module):
 
     def __init__(self,
-                d_model: int = 512,
-                num_heads: int = 8,
-                drop_out: float = 0.1):
+                d_model: int,
+                num_heads: int,
+                drop_out: float):
         super().__init__()
         self.d_model = d_model
         self.num_heads =  num_heads
@@ -98,7 +98,7 @@ class MultiHeadAttention(nn.Module):
         b, seq_len, d_model = x.shape
         # x = x.reshape(b, self.num_heads, seq_len, self.head_dim)
         x = x.reshape(b, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
-        print(f"{x.shape = }")
+        # print(f"{x.shape = }")
 
         attention = Attention(d_k = self.head_dim)
         x = attention(x, x, x, mask)
@@ -107,13 +107,90 @@ class MultiHeadAttention(nn.Module):
         x = self.w_0(x)
         return self.drop_out(x)
 
+class FeedForward(nn.Module):
+
+    def __init__(self,
+                d_model: int,
+                ff_dim: int,
+                drop_out: float):
+        super().__init__()
+        self.hidden_layer = nn.Linear(d_model, ff_dim)
+        self.out_layer = nn.Linear(ff_dim, d_model)
+        self._drop_out = nn.Dropout(drop_out)
+    
+    def forward(self, x):
+        x = self.hidden_layer(x)
+        x = self.out_layer(x)
+        return self._drop_out(x)
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, 
+                d_model:int,
+                ff_dim: int, 
+                num_head: int,
+                drop_out: float):
+        super().__init__()
+        self.multi_head_attention = MultiHeadAttention(
+            d_model=d_model,
+            num_heads=num_head,
+            drop_out=drop_out
+        )
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.ff_layer = FeedForward(
+            d_model=d_model,
+            ff_dim=ff_dim,
+            drop_out=drop_out
+        )
+    
+    def forward(self, x):
+        residual = x #(B, Seq_len,, d_model)
+        x = self.multi_head_attention(x)
+        x += residual
+        x = self.layer_norm(x)
+
+        residual = x
+        x = self.ff_layer(x)
+        x += residual
+        x = self.layer_norm(x)
+
+        return x      
+
+class Encoder(nn.Module):
+
+    def __init__(self,
+                num_encoder_block: int,
+                d_model: int,
+                ff_dim: int,
+                num_head: int,
+                drop_out: int):
+        super().__init__()
+        self.layers = nn.ModuleList([EncoderBlock(d_model, ff_dim, num_head, drop_out) 
+                                    for _ in range(num_encoder_block)])
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
 
 if __name__ == "__main__":
+    vocab_size = 30000
+    max_seq_len = 200
+    d_model = 512
+    ff_dim = 2048
+    num_head = 8
+    drop_out = 0.1
+
+    num_encoder_block = 6
+
+
     fake_data = torch.randint(low=0, high=10, size=(1, 10))
 
     embedding = nn.Sequential(
-        InputEmbedding(vocab_size=30000, d_model=512),
-        PositionEmbedding(max_seq_len=350, d_model=512)
+        InputEmbedding(vocab_size=vocab_size, d_model=d_model),
+        PositionEmbedding(max_seq_len=350, d_model=d_model, drop_out=drop_out)
     )
 
     fake_data_embeded = embedding(fake_data)
@@ -121,5 +198,14 @@ if __name__ == "__main__":
     # print(f"{type(fake_data_embeded)}")
     # print(f"{fake_data_embeded.shape}")
     
-    multi_head_attention = MultiHeadAttention()
-    out = multi_head_attention(fake_data_embeded)
+    
+    encoder = Encoder(
+        num_encoder_block=num_encoder_block,
+        d_model=d_model,
+        ff_dim=ff_dim,
+        num_head=num_head,
+        drop_out=drop_out
+    )
+
+    out = encoder(fake_data_embeded)
+    print(out.shape)
